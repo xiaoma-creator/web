8292ecec6a7f003d7ebec4f9b99f41cebc4a8022
yinwenfeng-company <2209041586@qq.com>
2022-11-01 14:20:21 +0800

    1. jq显示和隐藏div
    2. html '<>'里面是字母不会显示

--- js/test.js
+++ js/test.js
@@ -1,18 +1,34 @@
 
 $(document).ready(function(){
     
-    $("#btn_select").click(function(){
+    // --------1.下拉选项
+    $("#btn_select_1").click(function(){
         console.log("set b");
-        $("#select_abc").val(3);
+        $("#select_abc_1").val(3);
     });
 
-    $("#select_abc").val("b");
+    $("#select_abc_1").val("b");
 
+    // 选项改变触发动作
+    $("#select_abc_1").change(function () { 
+        console.log("select changed");
+        
+    });
+    
     // --------------2.输入框赋值
     $("#name").val("yin wen feng");
 
     $("#btn_applay_2").click(function () {
         console.log("content:%s", $("#name").val());
+    });
+
+    //----------------3.jq显示和隐藏div
+    $("#btn_show_3").click(function () {
+        $("#show_and_hidden_3").show();
+    });
+
+    $("#btn_hide_3").click(function () { 
+        $("#show_and_hidden_3").hide();
     });
 });
 
